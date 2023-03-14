13d552341fea9d4c5732744b55b41b0f61aae8dc
justadirck <justadirck@gmail.com>
2023-03-14 07:30:48 -0400

    apache geode impl

--- gemfire/src/main/java/boot/Application.java
+++ gemfire/src/main/java/boot/Application.java
@@ -1,9 +1,20 @@
 package boot;
 
+import boot.model.Person;
+import org.apache.geode.cache.client.ClientRegionShortcut;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.gemfire.config.annotation.ClientCacheApplication;
+import org.springframework.data.gemfire.config.annotation.EnableEntityDefinedRegions;
+import org.springframework.data.gemfire.repository.config.EnableGemfireRepositories;
 
 @SpringBootApplication
+@ClientCacheApplication(name = "AccessingGemFireDataRestApplication")
+@EnableEntityDefinedRegions(
+  basePackageClasses = Person.class,
+  clientRegionShortcut = ClientRegionShortcut.LOCAL
+)
+@EnableGemfireRepositories
 public class Application {
 
 	public static void main(String[] args) {
