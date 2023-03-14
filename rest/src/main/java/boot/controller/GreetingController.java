eaece5649aaf93098de98c356c098c00ce0cc5ab
justadirck <justadirck@gmail.com>
2023-03-14 07:36:13 -0400

    added rest client tests

--- rest/src/main/java/boot/controller/GreetingController.java
+++ rest/src/main/java/boot/controller/GreetingController.java
@@ -10,11 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    private static final String template = "hello, %s!";
+    private static final String template = "hello %s";
 	private final AtomicLong counter = new AtomicLong();
     
     @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "world") String name) {
 
       return new Greeting(this.counter.incrementAndGet(), String.format(template,name));
 
