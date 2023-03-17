6cadb56162f643b8b96181e6500c1fb848e31709
justadirck <justadirck@gmail.com>
2023-03-17 14:57:37 -0400

    cleanup

--- mysql/src/main/java/boot/Application.java
+++ mysql/src/main/java/boot/Application.java
@@ -2,12 +2,20 @@ package boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		
+		ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+		RepositoryRestConfiguration repositoryRestConfiguration = ctx.getBean(RepositoryRestConfiguration.class);
+
+        repositoryRestConfiguration.setReturnBodyForPutAndPost(true);
+		
 	}
 
 }
--- mysql/src/main/java/boot/PersonRepository.java
+++ mysql/src/main/java/boot/PersonRepository.java
@@ -1,7 +1,9 @@
 package boot;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
 public interface PersonRepository extends CrudRepository<Person, Integer> {
 
 }
