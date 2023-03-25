de2a0a07eacccf80ac7d280ba575498ca06939d5
Dirck Hecking <justadirck@gmail.com>
2023-03-25 15:21:01 -0400

    added email to person

--- mongo/src/main/java/boot/Person.java
+++ mongo/src/main/java/boot/Person.java
@@ -1,7 +1,9 @@
 package boot;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 
+@Data
 public class Person {
 
     @Id
@@ -9,20 +11,6 @@ public class Person {
 
     private String firstName;
     private String lastName;
+    private String email;
 
-    public String getFirstName() {
-        return this.firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return this.lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 }
