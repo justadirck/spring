f2f7846f9c52b31af00a7a7f397c9f4315276c1e
Dirck Hecking <justadirck@gmail.com>
2023-03-26 13:30:03 -0400

    another iteration

---
+++ sqlserver/src/main/java/boot/AppController.java
@@ -0,0 +1,15 @@
+package boot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+	@GetMapping("/")
+	public String root( String name, Model model) {
+		return "sqlserver";
+	}
+
+}
