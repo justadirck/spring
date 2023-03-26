83f2e7a24cd811863dbba1c42835fb6f07ab7a10
Dirck Hecking <justadirck@gmail.com>
2023-03-26 14:48:45 -0400

    sqlserver returns

--- sqlserver/src/main/java/boot/AppController.java
+++ sqlserver/src/main/java/boot/AppController.java
@@ -1,15 +1,16 @@
 package boot;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AppController {
 
-	@GetMapping("/")
-	public String root( String name, Model model) {
-		return "sqlserver";
+	@ResponseBody
+	@GetMapping(path = "/")
+	public String root() {
+		return "sqlserver ok";
 	}
 
 }
